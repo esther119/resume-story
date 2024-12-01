dfd1cebde715a9fd8cacbaa7d652ffcf773e2698
swimmingcircle119 <lampmaa22@gmail.com>
2024-12-01 17:55:33 -0500

    add experience

--- backend/app.py
+++ backend/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/experience', methods=['POST'])
+def get_experience(experience: str):
+    return experience
+
 if __name__ == '__main__':
     app.run(debug=True)
